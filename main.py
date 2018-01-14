69cfdf3e591ac7246ee2748dbe6fa847fc2f3c23
Parveen Sumal <vtvanshthakur35@gmail.com>
2018-01-14 20:22:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-name=input("enter the name of person: ")
-name=name.split()
-leng=len(name)
-last=name[leng-1]
-last=last.capitalize()
-first=""
-for i in range(0,leng-1):
-    temp=""
-    temp=name[i]
-    temp=temp[0].capitalize()+". "
-    first+=temp
-name=first+" "+last
-print(name)
+choice=True
+def main():
+    name=input("enter the name of person: ")
+    name=name.split()
+    leng=len(name)
+    last=name[leng-1]
+    last=last.capitalize()
+    first=""
+    for i in range(0,leng-1):
+        temp=""
+        temp=name[i]
+        temp=temp[0].capitalize()+". "
+        first+=temp
+    name=first+" "+last
+    print(name)
+    choice=input("False for exit, Anything else will continue the program\n")
+while(choice!=False):
+    main()
